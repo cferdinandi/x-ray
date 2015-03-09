3a208eca6313c6d981f7384d5270816cfcdc9c9f
Chris Ferdinandi <hello@gomakethings.com>
2015-03-08 23:05:17 -0400

    Fixed UMD wrapper

--- src/js/x-ray.js
+++ src/js/x-ray.js
@@ -1,12 +1,12 @@
 (function (root, factory) {
 	if ( typeof define === 'function' && define.amd ) {
-		define('xray', factory(root));
+		define([], factory);
 	} else if ( typeof exports === 'object' ) {
-		module.exports = factory(root);
+		module.exports = factory;
 	} else {
 		root.xray = factory(root);
 	}
-})(this, function (root) {
+})(this, function (window) {
 
 	'use strict';
 
@@ -15,7 +15,7 @@
 	//
 
 	var xray = {}; // Object for public APIs
-	var supports = !!document.querySelector && !!root.addEventListener; // Feature test
+	var supports = !!document.querySelector && !!window.addEventListener; // Feature test
 	var settings, toggles;
 
 	// Default settings
